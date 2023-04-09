af218bcfd7912495f9ac386d2f31b3934617f79a
Zouhair Fouiguira <zouhairfgra@gmail.com>
2023-04-09 22:00:00 +0000

    first commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>iPhone Facture</title>
+    <title>iPhone Register</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
